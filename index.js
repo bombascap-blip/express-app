c40943a0842a00da9fd057131c952871f6ee49a1
bombascap-blip <bombascap-blip@users.noreply.github.com>
2026-05-05 21:44:23 +0300

    Fix syntax error in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require('express') 
-const app = express() 
-app.get('/', (req, res) = from CI/CD')) 
-app.listen(3000, () = listening on port 3000')) 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD!');
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
